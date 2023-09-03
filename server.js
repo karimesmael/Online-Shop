7b1356be929613322b2b84781e3b69eb4e48e1df
karim <karimesma3el407@gmail.com>
2023-09-03 18:49:09 +0300

    updated

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.use((error, req, res, next) => {
   });
 });
 mongoose
-  .connect(`${process.env.MONGODB_URI}`, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
